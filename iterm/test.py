6d368cbc80fceff44127cff841ed7efc7825badb
Patrick Canny <patrickcanny@ku.edu>
2019-02-09 18:59:33 -0600

    websock

--- iterm/test.py
+++ iterm/test.py
@@ -1,29 +1,62 @@
 import argparse
+import subprocess
 import os
 import pty
 import sys
 import time
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-a', dest='append', action='store_true')
-parser.add_argument('-p', dest='use_python', action='store_true')
-parser.add_argument('filename', nargs='?', default='typescript')
-options = parser.parse_args()
+def get_input():
+    command = input("What is your command? ")
+    return command
 
-shell = sys.executable if options.use_python else os.environ.get('SHELL', 'sh')
-filename = options.filename
-mode = 'ab' if options.append else 'wb'
+def on_message(ws, message):
+    print("Message: %s" % message)
+    commands = [message]
+    subprocess.run(commands)
+    ws.close()
 
-# pty stuff
-with open(filename, mode) as script:
-    def read(fd):
-        data = os.read(fd, 1024)
-        script.write(data)
-        return data
+def on_error(ws, error):
+    print(error)
 
-    print('Script started, file is',filename)
-    script.write(('Script started on %s\n' % time.asctime()).encode())
+def on_close(ws):
+    print("### closed ###")
 
-    pty.spawn(shell, read)
-    script.write(('Script done on %s\n' % time.asctime()).encode())
-    print('Script done, file is', filename)
+def on_open(ws):
+    def run(*args):
+        command = get_input()
+        ws.send(command)
+
+    thread.start_new_thread(run, ())
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://echo.websocket.org/",on_message =
+            on_message, on_error = on_error, on_close = on_close)
+
+    ws.on_open = on_open
+    ws.run_forever()
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-a', dest='append', action='store_true')
+# parser.add_argument('-p', dest='use_python', action='store_true')
+# parser.add_argument('filename', nargs='?', default='typescript_2')
+# options = parser.parse_args()
+# filename = options.filename
+# mode = 'ab' if options.append else 'wb'
+
+
+
+# # pty stuff
+# with open(filename, mode) as script:
+#     print('Script started, file is',filename)
+#     script.write(('Script started on %s\n' % time.asctime()).encode())
+#     ws = create_connection('ws://echo.websocket.org/')
+#     ws.send('bash-3.2$ echo "Hello, World"\n')
+#     pty.spawn(shell, read)
+#     script.write(('Script done on %s\n' % time.asctime()).encode())
+#     print('Script done, file is', filename)
